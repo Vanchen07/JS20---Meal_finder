4cb0f03c819bef8d75f8876a0485633753e758d8
Vanessa Chen <vanchen07@gmail.com>
2020-02-22 15:16:34 -0800

    create js file and grabbed dom elements

---
+++ script.js
@@ -0,0 +1,15 @@
+const search = document.getElementById('search'),
+    submit = document.getElementById('submit'),
+    random = document.getElementById('random'),
+    mealsEl = document.getElementById('meals'),
+    resultHeading = document.getElementById('result-heading'),
+    single_mealEl = document.getElementById('single-meal');
+
+
+funtion searchMeal(e) {
+    e.preventDefault();
+
+    
+}
+
+submit.addEventListener('submit', searchMeal);
