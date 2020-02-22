d4e741e8fb5d643597a4c926c1631e73797b7470
Vanessa Chen <vanchen07@gmail.com>
2020-02-22 15:48:49 -0800

    create event listener on submit button

--- script.js
+++ script.js
@@ -6,10 +6,39 @@ const search = document.getElementById('search'),
     single_mealEl = document.getElementById('single-meal');
 
 
-funtion searchMeal(e) {
+function searchMeal(e) {
     e.preventDefault();
 
-    
+    single_mealEl.innerHTML = '';
+
+    const term = search.value;
+
+    if (term.trim()) {
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
+
+                if (data.meals === null) {
+                    resultHeading.innerHTML = `<p>There are no search results. Try again!</p>`;
+                } else {
+                    mealsEl.innerHTML = data.meals.map((meal) => {
+                        return (
+                       ` <div class="meal">
+                            <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+                            <div class="meal-info" data-mealID="${meal.idMeal}">
+                                <h3>${meal.strMeal}</h3>
+                            </div>
+                        </div>
+                        `
+                    )}).join('');
+                }
+            });
+        search.value = '';
+    } else {
+        alert('Please enter search term');
+    }
 }
 
 submit.addEventListener('submit', searchMeal);
